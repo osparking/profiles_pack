e0d52325b5dc178d09aa644a46bec6a344aad0b5
osparking <jbpark03@gmail.com>
2023-07-24 15:29:17 +0900

    spring web, spring boot 2.7.*, init

---
+++ src/main/java/space/bumsoap/profile/ProfilesPackApplication.java
@@ -0,0 +1,13 @@
+package space.bumsoap.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilesPackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilesPackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/bumsoap/profile/ProfilesPackApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bumsoap.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilesPackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
